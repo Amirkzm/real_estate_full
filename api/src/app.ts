37a135ce34bdcbfdba31c5cb87cbf4c65509fbf9
Amir <kazemi.amir68@gmail.com>
2024-07-28 10:04:29 +0200

    chore: -Update npm dependencies and add cookie-parser and zod packages - use errormiddleware and cookie-parse in app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import postRouter from "./routes/post.route";
 import authRouter from "./routes/auth.route";
+import testRouter from "./routes/test.route";
+import cookieParser from "cookie-parser";
+import { errorMiddleWare } from "./middleware/error";
 
 dotenv.config();
 
@@ -12,13 +15,16 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/test", testRouter);
+
+app.use(errorMiddleWare);
 
 app.listen(port, () => {
   console.log(`Server is runnin on http://localhost:${port}`);
 });
-console.log("jwt is ", process.env.JWT_SECRET);
