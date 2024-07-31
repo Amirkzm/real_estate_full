2f74c962fa2371f6d8a35803f4818c0beaa6a187
Amir <kazemi.amir68@gmail.com>
2024-07-31 21:44:05 +0200

    chore: Add react-quill dependency and update variables.scss

--- api/src/utils/errorHandler.ts
+++ api/src/utils/errorHandler.ts
@@ -1,17 +1,22 @@
 import { ZodError } from "zod";
 import HttpException from "../exceptions/httpException";
 import InternalException from "../exceptions/internalException";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export const errorHandler = (func: Function) => {
   return async (req: any, res: any, next: any) => {
     try {
       await func(req, res);
     } catch (error) {
-      let exception: HttpException | ZodError;
+      let exception:
+        | HttpException
+        | ZodError
+        | Error
+        | PrismaClientKnownRequestError;
       if (error instanceof HttpException || error instanceof ZodError) {
         exception = error;
       } else {
-        console.log("error", error);
+        console.log("internar error details::", error);
         exception = new InternalException();
       }
 
