406aa80ac6551637986ccf1aa49327962df1aefa
Amir <kazemi.amir68@gmail.com>
2024-08-05 12:46:36 +0200

    refactor: Update errorExtractor to handle additional error message format

--- frontend/src/lib/ErrorExtractor.ts
+++ frontend/src/lib/ErrorExtractor.ts
@@ -1,13 +1,16 @@
 import { AxiosError } from "axios";
 
-export const errorExtractor = (err: AxiosError | Error) => {
+export const errorExtractor = (err: AxiosError | Error): string => {
+  console.log("raw err in errorExtractor", err);
   if (err instanceof AxiosError) {
     if (err.response) {
-      const errorDetails = err.response?.data?.errors?.details;
-      console.log(errorDetails);
+      const errorDetails =
+        err.response?.data?.errors?.details ||
+        err.response?.data?.errors?.message;
 
       return errorDetails[0].path[0] + " " + errorDetails[0].message;
     } else {
+      console.log(err.message);
       return err.message;
     }
   } else {
