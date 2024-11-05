56424d7b711bce9308c42c77f68ae0549daaefcb
amir.kzm <kazemi.amir68@gmail.com>
2024-11-05 16:37:59 +0100

    refactor: Update homepage description for clarity and quality assurance

--- frontend/src/pages/homePage/Homepage.tsx
+++ frontend/src/pages/homePage/Homepage.tsx
@@ -12,9 +12,9 @@ const Homepage = () => {
         <div className="wrapper">
           <h1>Find Real State & Get Your Dream Place</h1>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Voluptatibus et tempore nemo itaque reiciendis ratione ullam
-            sapiente repellat enim dicta obcaecati.
+            Lorem ipsum dolor sit, Ensure the quality. Voluptatibus et tempore
+            nemo itaque reiciendis ratione ullam sapiente repellat enim dicta
+            obcaecati.
           </p>
           <SearchBar />
 
