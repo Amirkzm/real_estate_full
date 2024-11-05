fa4bad7df869e87417fff51a23c9425a73f2a774
amir.kzm <kazemi.amir68@gmail.com>
2024-11-05 18:42:04 +0100

    fix: Improve wording in homepage description for clarity

--- frontend/src/pages/homePage/Homepage.tsx
+++ frontend/src/pages/homePage/Homepage.tsx
@@ -12,7 +12,7 @@ const Homepage = () => {
         <div className="wrapper">
           <h1>Find Real State & Get Your Dream Place</h1>
           <p>
-            Join us to explore list of your dream places, Ensure the quality.
+            Join us and explore list of your dream places, Ensure the quality.
             Voluptatibus et tempore nemo itaque reiciendis ratione ullam
             sapiente repellat enim dicta obcaecati.
           </p>
