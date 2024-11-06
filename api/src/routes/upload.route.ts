9a54bc2c155b84ae52d1237d2d7b901c9289bad0
amir.kzm <kazemi.amir68@gmail.com>
2024-11-06 18:02:28 +0100

    fix: remove token verification from file upload route for simplified access

--- api/src/routes/upload.route.ts
+++ api/src/routes/upload.route.ts
@@ -2,11 +2,10 @@
 import { Router } from "express";
 import { errorHandler } from "../utils/errorHandler";
 import { getFile } from "../controllers/upload.controller";
-import { verifyToken } from "../middleware/verifyToken"; // Optional for protected access
 
 const router = Router();
 
 // Route to serve static files from 'uploads' folder
-router.get("/:filename", verifyToken, errorHandler(getFile));
+router.get("/:filename", errorHandler(getFile));
 
 export default router;
